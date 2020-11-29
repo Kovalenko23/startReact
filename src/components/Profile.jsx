40cc40813c0e7cd97238a9a9817d02e3c80e1e1b
WhatIsLove.K <vlad.kovaleko.od@hmail.com>
2020-11-29 19:30:52 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+const Profile = () => {
+return <div className = 'content'>
+  <div>
+  <img src ='https://i.pinimg.com/originals/89/5b/8e/895b8e6ffce7b37a22440ad79a3865a4.jpg' />
+  </div>
+  <div>
+    ava + description
+  </div>
+  <div>
+    my posts
+    <div>New posts </div>
+    <div>
+      post 1
+    </div>
+    <div> 
+      post 2
+    </div>
+  </div>
+  </div>
+}
+
+
+export default Profile;
